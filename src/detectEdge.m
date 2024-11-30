122110656c8a0691cb601177fa3de903d8bb695e
Muhammad Equilibrie Fajria <104043362+MuhLibri@users.noreply.github.com>
2024-11-30 17:17:09 +0700

    feat: fix edge detection for operator other than Canny

--- src/detectEdge.m
+++ src/detectEdge.m
@@ -1,39 +1,49 @@
 function img = detectEdge(image, type, threshold, sigma)
     if (nargin < 3)
         threshold = [];
-        sigma = 1;
+        sigma = 0;
     end
     
+    % Convert to gray if image is RGB    
     if size(image, 3) == 3
         image = rgb2gray(image);
     end
     
+    % Apply gaussian filter to the image only if sigma > 0 and operator not Canny or LoG    
+    if sigma > 0 && type ~= "Canny" && type ~= "LoG"
+        image = imgaussfilt(image, sigma);
+    end
+    
     switch type
-        case 'Laplace'
+        case "Laplace"
             H = [1 1 1; 1 -8  1; 1  1  1];
-            img = imbinarize(uint8(convn(double(image), double(H))));
-        case 'LoG'
-            H = fspecial('log');
-            img = imbinarize(uint8(convn(double(image), double(H))));            
-        case 'Sobel'
+            img = imbinarize(uint8(conv2(double(image), double(H), 'same')));
+        case "LoG"
+            H = fspecial('log', 5, sigma);
+            img = imbinarize(uint8(conv2(double(image), double(H), 'same')));            
+        case "Sobel"
             Sx = [-1 0 1; -2 0 2; -1 0 1];
             Sy = [1 2 1; 0 0 0; -1 -2 -1];
             Jx = conv2(double(image), double(Sx), 'same');
             Jy = conv2(double(image), double(Sy), 'same');
             img = imbinarize(uint8(sqrt(Jx.^2 + Jy.^2)));
-        case 'Prewitt'
+        case "Prewitt"
             Px = [-1 0 1; -1 0 1; -1 0 1];
             Py = [-1 -1 -1; 0 0 0; 1 1 1];
             Jx = conv2(double(image), double(Px), 'same');
             Jy = conv2(double(image), double(Py), 'same');
             img = imbinarize(uint8(sqrt(Jx.^2 + Jy.^2)));
-        case 'Roberts'
+        case "Roberts"
             Rx = [1 0; 0 -1];
             Ry = [0 1; -1 0];
             Jx = conv2(double(image), double(Rx), 'same');
             Jy = conv2(double(image), double(Ry), 'same');
             img = imbinarize(uint8(sqrt(Jx.^2 + Jy.^2)));
-        case 'Canny'
+        case "Canny"
             img = edge(image, "Canny", threshold, sigma);
     end
+
+    % Set the outermost pixel to 0            
+    img([1, end], :) = 0;
+    img(:, [1, end]) = 0;
 end
--- src/segmentation.m
+++ src/segmentation.m
@@ -4,20 +4,17 @@ function segmented_img = segmentation(image, type, radius, threshold, sigma)
         sigma = 1;
     end
 
+    % Perform edge detection
     edge_img = detectEdge(image, type, threshold, sigma);
-    figure; imshow(edge_img), title(type);
 
-    % Perform image linking (Closing the gap in the edges)
+    % Perform edge linking (Closing the gap in the edges)
     se = strel('disk', radius);
     closed_edges = imclose(edge_img, se);
-
-    figure; imshow(closed_edges), title("Closed edges");
     
     %  Filling the closed area   
     mask = imfill(closed_edges, 'holes');
-    figure; imshow(mask), title("mask");
     
-    % Apply Mask
+    % Apply mask to the image to get segmented image
     if size(image, 3) == 3
         red_processed = image(:,:,1).*uint8(mask);
         green_processed = image(:,:,2).*uint8(mask);
